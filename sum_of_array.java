7243bea1e197764d1379aa84d410163b7de5455e
chavipanwar2601-oss <chavi.panwar2601@gmail.com>
2026-06-19 17:23:52 +0530

    Add files via upload

---
+++ sum_of_array.java
@@ -0,0 +1,9 @@
+public class sum_of_array {
+    public static void main(String[] args) {
+        int[] arr ={1,3,4,5};
+        int sum =0;
+        for(int i=0;i<4;i++){
+            sum += arr[i];
+        }System.out.println(sum);
+    }
+}
