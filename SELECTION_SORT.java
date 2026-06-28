ef8264e8f2f99592c618d1a55fb4b65458635535
chavipanwar2601-oss <chavi.panwar2601@gmail.com>
2026-06-28 23:32:08 +0530

    Add files via upload

---
+++ SELECTION_SORT.java
@@ -0,0 +1,26 @@
+public class SELECTION_SORT{
+    public static void print(int[]arr){
+        for(int ele:arr){
+            System.out.print(ele + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int[] arr ={8,4,1,9,-3,6,5};
+        int n = arr.length;
+       print(arr);
+        for(int i =0;i<n-1;i++){
+            int min = Integer.MAX_VALUE , mindx=-1;
+            for(int j=i;j<n;j++){
+                if(arr[j]<min){
+                    min = arr[j];
+                    mindx =j;
+
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[mindx];
+            arr[mindx] =temp;
+        }print(arr);
+    }
+}
